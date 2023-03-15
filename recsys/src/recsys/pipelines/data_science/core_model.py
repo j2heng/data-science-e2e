9ef6e3e95115c8f64f1ed6d9a78ab55f0dd9da23
j2heng <zhengjie0706@gmail.com>
2023-03-15 16:12:25 -0700

    add minmax to CF

--- recsys/src/recsys/pipelines/data_science/core_model.py
+++ recsys/src/recsys/pipelines/data_science/core_model.py
@@ -1,16 +1,17 @@
 import torch
 import torch.nn as nn
 
+
 class CollaborativeFiltering(nn.Module):
-    def __init__(self, n_users, n_items, emb_size=512):
+    def __init__(self, n_users, n_movies, emb_size=512):
         super().__init__()
-        self.user_emb = nn.Embedding(n_users, emb_size, sparse=True)
-        self.item_emb = nn.Embedding(n_items, emb_size, sparse=True)
-        self.fc1 = nn.Linear(emb_size*2, 256)
+        self.user_emb = nn.Embedding(n_users, emb_size)
+        self.item_emb = nn.Embedding(n_movies, emb_size)
+        self.fc1 = nn.Linear(emb_size * 2, 256)
         self.fc2 = nn.Linear(256, 64)
         self.fc3 = nn.Linear(64, 1)
 
-    def forward(self, user_ids, item_ids):
+    def forward(self, user_ids, item_ids, minmax=None):
         user_embedding = self.user_emb(user_ids)
         item_embedding = self.item_emb(item_ids)
         x = torch.cat([user_embedding, item_embedding], dim=1)
@@ -20,19 +21,22 @@ class CollaborativeFiltering(nn.Module):
         x = nn.ReLU()(x)
         x = self.fc3(x)
         x = nn.Sigmoid()(x)
+        if minmax is not None:
+            min_rating, max_rating = minmax
+            x = x * (max_rating - min_rating + 1) + min_rating - 0.5
         return x
-    
+
     def predict(self, user_ids, item_ids):
-        return self.forward(user_ids, item_ids) 
-    
+        return self.forward(user_ids, item_ids)
+
 
 class MatrixFactorization(nn.Module):
-    def __init__(self, n_users, n_items, emb_size=512):
+    def __init__(self, n_users, n_movies, emb_size=512):
         super().__init__()
         self.user_emb = nn.Embedding(n_users, emb_size, sparse=True)
-        self.item_emb = nn.Embedding(n_items, emb_size, sparse=True)
+        self.item_emb = nn.Embedding(n_movies, emb_size, sparse=True)
         self.user_biases = nn.Embedding(n_users, 1)
-        self.item_biases = nn.Embedding(n_items, 1)
+        self.item_biases = nn.Embedding(n_movies, 1)
 
     def forward(self, user_ids, item_ids):
         user_embedding = self.user_emb(user_ids)
@@ -41,10 +45,8 @@ class MatrixFactorization(nn.Module):
         item_bias = self.item_biases(item_ids)
         dot_product = torch.sum(torch.mul(user_embedding, item_embedding), dim=1)
         output = dot_product + user_bias.squeeze() + item_bias.squeeze()
-        
+
         return output.squeeze()
-    
+
     def predict(self, user_ids, item_ids):
-        return self.forward(user_ids, item_ids) 
-
-
+        return self.forward(user_ids, item_ids)
